0363b12c689842ca0c0734aba91624b67a31a261
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-10-19 12:23:30 +0300

    Addning a tweets section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import HomePage from "./components/HomePage";
+import Tweets from "./components/Tweets";
 
 function App() {
   return (
     <div >
      <HomePage />
+     <Tweets />
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,7 +19,7 @@ function HomePage() {
             </div>
             <div>
                 <ul className='text-lg text-bold flex space-x-6'>
-                    <li className='text-extrabold '>HOME</li>
+                    <li >HOME</li>
                     <li>ABOUT ME</li>
                     <li>PROGRAMS</li>
                     <li>BLOGS</li>
@@ -29,7 +29,7 @@ function HomePage() {
 
         </div>
         <div className=' relative h-[47rem] w-screen bg-[url("https://images.pexels.com/photos/8874550/pexels-photo-8874550.jpeg?auto=compress&cs=tinysrgb&w=1600")] bg-cover  bg-right'>
-            <div className=' w-[30rem] text-white absolute top-56 right-14 '>
+            <div className=' w-[30rem] text-white absolute top-52 right-14 '>
                 <h1 className='text-6xl tracking-wider font-black'><span className='text-yellow-300 font-light'>TRANSFORM </span>YOUR LIFE...</h1>
                 <p className='leading-7 mt-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
