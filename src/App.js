80c070f112fff402a34b0a5326a1d4f4863d9919
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-10-20 16:27:11 +0300

    put a footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./components/HomePage";
 import Main from "./components/Main";
 import MainContent from "./components/MainContent";
 import Tweets from "./components/Tweets";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Main />
      <MainContent />
      <Summary />
+     <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='h-14 w-screen bg-black text-white text-center pt-3 '>SAMUEL MUYA @ 2022  | Privacy Policy</div>
+  )
+}
+
+export default Footer;
