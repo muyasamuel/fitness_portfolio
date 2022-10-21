0ef86c73175da03225168af49d6e99e1bd19ca8e
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-10-21 23:31:06 +0300

    add responsiveness to thehomepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,12 +13,12 @@ function HomePage() {
         </div>
         <div className='bg-slate-100  h-24 w-screen  flex items-center justify-around '>
             <div>
-                <h1 className='text-5xl font-bold  '> SAM <span className='text-red-500'>NJOMO</span></h1>
+                <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold  '> SAM <span className='text-red-500'>NJOMO</span></h1>
                 <p className='tracking-widest text-slate-900'>PERSONAL TRAINER</p>
 
             </div>
             <div>
-                <ul className='text-lg text-bold flex space-x-6'>
+                <ul className='text-sm md:text-base lg:text-lg text-bold flex space-x-3 md:space-x-6'>
                     <li >HOME</li>
                     <li>ABOUT ME</li>
                     <li>PROGRAMS</li>
@@ -30,8 +30,8 @@ function HomePage() {
         </div>
         <div className=' relative h-[47rem] w-screen bg-[url("https://images.pexels.com/photos/8874550/pexels-photo-8874550.jpeg?auto=compress&cs=tinysrgb&w=1600")] bg-cover  bg-right'>
             <div className=' w-[30rem] text-white absolute top-52 right-14 '>
-                <h1 className='text-6xl tracking-wider font-black'><span className='text-red-500 font-light'>TRANSFORM </span>YOUR LIFE...</h1>
-                <p className='leading-7 mt-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                <h1 className='text-3xl md:text-5xl lg:text-6xl tracking-wider font-black'><span className='text-red-500 font-light'>TRANSFORM </span>YOUR LIFE...</h1>
+                <p className='leading-4 md:leading-6 lg:leading-8 mt-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                       enim ad minim veniam, quis nostrud exercitation ullamco laboris
                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
@@ -40,7 +40,7 @@ function HomePage() {
                        sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                        <button className='bg-red-500 hover:bg-red-700 py-4 px-8 mt-6 rounded-xl '>READ MORE </button>
             </div>
-
+ 
         </div>
     </>
   )
